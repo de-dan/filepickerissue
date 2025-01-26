6390749c9167c13b78ca8ea01b80bdc6c66db934
de-dan <d.kasper@gmx.net>
2025-01-26 20:17:25 +0100

    add android and filepicker plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, signal } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import {FilePicker, PickedFile, PickFilesResult} from '@capawesome/capacitor-file-picker';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, JsonPipe],
 })
 export class HomePage {
   constructor() {}
+
+  result = signal<Partial<PickFilesResult>>({});
+  error = signal<any>(undefined);
+
+  async pickFile() {
+    console.log("pick file button clicked");
+    this.error.set(undefined);
+    try {
+      const files = await FilePicker.pickFiles({
+        readData: true,
+      });
+
+      console.log(files);
+      this.result.set(files);
+    } catch (e) {
+      this.error.set(e);
+      this.result.set({});
+    }
+  }
+
+  prettyFile(file: PickedFile) {
+    return {...file, data: file.data?.substring(0, 100)}
+  }
 }
